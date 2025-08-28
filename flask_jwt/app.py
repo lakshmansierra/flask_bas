5aa9036639081ad2807438a00cd11dc653573adf
Lakshman <lakshman1122000@gmail.com>
2025-08-28 13:29:44 +0530

    commited

--- flask_jwt/app.py
+++ flask_jwt/app.py
@@ -3,16 +3,11 @@ from flask import Flask, request, jsonify
 from flask_jwt_extended import (
     JWTManager, create_access_token, jwt_required, 
 )
-from db import stores
+from db import stores, users
 
 app = Flask(__name__)
 app.config["JWT_SECRET_KEY"] = "super-secret-key"  # change in production
 jwt = JWTManager(app)
-
-# Fake user database
-users = {
-    "admin": {"username": "admin", "password": "secret"}
-}
 
 # ---------------- AUTHENTICATION ----------------
 @app.post("/login")
