3a1367e52dd5800aaac62f5233074f544ad00e37
Lakshman <lakshman1122000@gmail.com>
2025-08-28 13:26:51 +0530

    commited

--- flask_jwt/app.py
+++ flask_jwt/app.py
@@ -1,7 +1,7 @@
 import uuid
 from flask import Flask, request, jsonify
 from flask_jwt_extended import (
-    JWTManager, create_access_token, jwt_required, get_jwt_identity
+    JWTManager, create_access_token, jwt_required, 
 )
 from db import stores
 
